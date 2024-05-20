7eb62330f48719022aafa13b902c1ff31b7fd481
leoespi <riveraleo113@gmail.com>
2024-05-20 17:24:51 -0500

    Nuevo diseño barra menu

--- lib/views/menu.dart
+++ lib/views/menu.dart
@@ -11,10 +11,9 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar (
-        
+      appBar: AppBar(
         actions: [
-          IconButton (
+          IconButton(
             icon: Icon(Icons.logout),
             onPressed: () => _authController.logout(),
           ),
@@ -22,15 +21,25 @@ class MenuPage extends StatelessWidget {
       ),
       
       bottomNavigationBar: Transform.translate(
-        offset: Offset(0.0, -5.0), // Ajusta el valor verticalmente según sea necesario
+        offset: Offset(0.0, -5.0),
         child: Container(
           decoration: BoxDecoration(
-            color: Color(0xFF3366FF), // Cambia aquí el color a azul rey
-            borderRadius: BorderRadius.circular(15.0),
-          
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
           ),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 1.0, vertical: 10.0),
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -42,16 +51,14 @@ class MenuPage extends StatelessWidget {
                       MaterialPageRoute(builder: (context) => MyForm()),
                     );
                   },
-                  color: Colors.white, // Cambia aquí el color del icono
-                  iconSize: 30, // Cambia aquí el tamaño del icono
+                  color: Colors.blue,
                 ),
                 _buildBottomMenuItem(
                   icon: Icons.quiz,
                   onPressed: () {
                     // Agrega aquí la lógica para la otra opción del menú
                   },
-                  color: Colors.white, // Cambia aquí el color del icono
-                  iconSize: 30, // Cambia aquí el tamaño del icono
+                  color: Colors.green,
                 ),
                 _buildBottomMenuItem(
                   icon: Icons.person,
@@ -65,8 +72,7 @@ class MenuPage extends StatelessWidget {
                       ),
                     );
                   },
-                  color: Colors.white, // Cambia aquí el color del icono
-                  iconSize: 30, // Cambia aquí el tamaño del icono
+                  color: Colors.orange,
                 ),
               ],
             ),
@@ -76,56 +82,37 @@ class MenuPage extends StatelessWidget {
     );
   }
 
-  Widget _buildMenuItem(BuildContext context, {
-    required Color color,
-    required IconData icon,
-    required String text,
-    required VoidCallback onTap,
-  }) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(15.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: Offset(0, 3),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, color: Colors.white, size: 40),
-            SizedBox(height: 10),
-            Text(
-              text,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   Widget _buildBottomMenuItem({
     required IconData icon,
     required VoidCallback onPressed,
     required Color color,
-    required double iconSize, // Añade el tamaño del icono
   }) {
-    return IconButton(
-      icon: Icon(icon, color: color, size: iconSize), // Aplica el tamaño del icono
-      onPressed: onPressed,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onPressed,
+        borderRadius: BorderRadius.circular(30),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Icon(
+            icon,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ),
     );
   }
 }

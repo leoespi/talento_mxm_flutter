8e2d1f28f481d1037872402894ac2a702fecea95
leoespi <riveraleo113@gmail.com>
2024-10-07 09:29:40 -0500

    token para publicacion_controller

--- lib/controllers/publicacion_controller.dart
+++ lib/controllers/publicacion_controller.dart
@@ -1,36 +1,38 @@
 import 'dart:convert';
 import 'package:talento_mxm_flutter/models/publicacion_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:get_storage/get_storage.dart';
 
 class FeedController {
   static const String baseUrl = 'http://10.0.2.2:8000/api';
   
- static Future<List<Publicacion>> obtenerFeeds(int offset, int limit) async {
-  try {
-    final response = await http.get(Uri.parse('$baseUrl/feeds?order=desc&offset=$offset&limit=$limit'));
+  static Future<List<Publicacion>> obtenerFeeds(int offset, int limit) async {
+    try {
+      final box = GetStorage();
+      String? token = box.read('token');
 
-    if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(response.body)['feeds'];
-      List<Publicacion> feeds = [];
+      final response = await http.get(
+        Uri.parse('$baseUrl/feeds?order=desc&offset=$offset&limit=$limit'),
+        headers: token != null ? {'Authorization': 'Bearer $token'} : {},
+      );
 
-      for (var item in data) {
-        feeds.add(Publicacion.fromJson(item));
+      if (response.statusCode == 200) {
+        List<dynamic> data = json.decode(response.body)['feeds'];
+        List<Publicacion> feeds = [];
+
+        for (var item in data) {
+          feeds.add(Publicacion.fromJson(item));
+        }
+
+        // Ordenar los feeds por id de manera descendente (como un entero)
+        feeds.sort((a, b) => b.id.compareTo(a.id));
+
+        return feeds;
+      } else {
+        throw Exception('Error en la solicitud: ${response.statusCode}');
       }
-
-      // Ordenar los feeds por id de manera descendente (como un entero)
-      feeds.sort((a, b) => b.id.compareTo(a.id));
-
-      return feeds;
-    } else {
-      throw Exception('Error en la solicitud: ${response.statusCode}');
+    } catch (e) {
+      throw Exception('Error de conexión: $e');
     }
-  } catch (e) {
-    throw Exception('Error de conexión: $e');
   }
-}
-
-
-
-
-
 }
